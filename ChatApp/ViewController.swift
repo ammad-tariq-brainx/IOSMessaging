76d4f4b2f4e7e69f89c65d770458aac088c43f5b
Ammad <ammad.tariq@brainxtech.com>
2019-08-08 18:36:07 +0500

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by BrainX IOS 4 on 2019-08-08.
+//  Copyright © 2019 BrainX Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
